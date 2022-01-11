9aada4784fc3c909eddf4c12214379645161bd5d
Elena Doroshenko <dor.ele.an@gmail.com>
2022-01-11 13:54:56 +0200

    Changes to be committed: 	new file:   .vs/DZ_4_Timer/v16/.suo 	new file:   DZ_4_Timer.sln 	new file:   DZ_4_Timer/App.config 	new file:   DZ_4_Timer/DZ_4_Timer.csproj 	new file:   DZ_4_Timer/Program.cs 	new file:   DZ_4_Timer/Properties/AssemblyInfo.cs 	new file:   DZ_4_Timer/bin/Debug/DZ_4_Timer.exe 	new file:   DZ_4_Timer/bin/Debug/DZ_4_Timer.exe.config 	new file:   DZ_4_Timer/bin/Debug/DZ_4_Timer.pdb 	new file:   DZ_4_Timer/obj/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs 	new file:   DZ_4_Timer/obj/Debug/DZ_4_Timer.csproj.AssemblyReference.cache 	new file:   DZ_4_Timer/obj/Debug/DZ_4_Timer.csproj.CoreCompileInputs.cache 	new file:   DZ_4_Timer/obj/Debug/DZ_4_Timer.csproj.FileListAbsolute.txt 	new file:   DZ_4_Timer/obj/Debug/DZ_4_Timer.exe 	new file:   DZ_4_Timer/obj/Debug/DZ_4_Timer.pdb 	new file:   DZ_4_Timer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

---
+++ DZ_4_Timer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZ_4_Timer
+{
+    class Program
+    {
+        /*
+         Используя таймер, написать код, выводящий значение времени по следующим правилам:
+	    - первые 10 секунд каждую секунду, первый вывод через 3 секунды после запуска программы;
+	    - вторые 10 секунд каждые 2 секунды.
+         */
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(TenSecondMethod, null, 3000, 1000);
+            Console.WriteLine("Таймер запущен, через 3 секунды начнется отсчет");
+            Thread.Sleep(10000);
+            Console.WriteLine("А сейчас таймер будет отображаться каждые 2 секунды");
+            timer.Change(0, 2000);
+            Thread.Sleep(10000);
+            timer.Dispose();
+
+            Console.ReadKey();
+        }
+
+        private static void TenSecondMethod(object state)
+        {
+            Console.WriteLine( $"{DateTime.Now.ToLongTimeString()}"); ;
+        }
+    }
+}
